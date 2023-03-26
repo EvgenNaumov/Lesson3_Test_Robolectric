f6bfee77488c234e161a2cad3afb1d012b9a9b53
Evgen Naumov <naumovev@mail.ru>
2023-03-26 23:45:18 +0300

    HW7 Добавил DetailsFragment.kt

--- app/src/main/java/com/geekbrains/tests/view/details/DetailsActivity.kt
+++ app/src/main/java/com/geekbrains/tests/view/details/DetailsActivity.kt
@@ -9,56 +9,34 @@ import com.geekbrains.tests.R
 import com.geekbrains.tests.presenter.details.DetailsPresenter
 import com.geekbrains.tests.presenter.details.PresenterDetailsContract
 import kotlinx.android.synthetic.main.activity_details.*
+import kotlinx.android.synthetic.main.fragment_details.*
 import java.util.*
 
-class DetailsActivity : AppCompatActivity(), ViewDetailsContract {
+class DetailsActivity : AppCompatActivity() {
 
     private lateinit var presenter: PresenterDetailsContract
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
-        presenter = lastCustomNonConfigurationInstance as? PresenterDetailsContract ?:DetailsPresenter()
-        presenter.onAttach(this)
-        setUI()
+        supportFragmentManager.beginTransaction()
+            .add(
+                R.id.detailsFragmentContainer,
+                DetailsFragment.newInstance(intent.getIntExtra(TOTAL_COUNT_EXTRA, 0))
+            )
+            .commitAllowingStateLoss()
 
-    }
-
-    override fun onRetainCustomNonConfigurationInstance(): PresenterDetailsContract {
-        return presenter
-    }
-    private fun setUI() {
-        val count = intent.getIntExtra(TOTAL_COUNT_EXTRA, 0)
-        presenter.setCounter(count)
-        decrementButton.setOnClickListener { presenter.onDecrement() }
-        incrementButton.setOnClickListener { presenter.onIncrement() }
-    }
-
-    override fun setCount(count: Int) {
-        setCountText(count)
-    }
-
-    private fun setCountText(count: Int) {
-        totalCountTextView.text =
-            String.format(Locale.getDefault(), getString(R.string.results_count), count)
     }
 
     companion object {
 
-        const val TOTAL_COUNT_EXTRA = "TOTAL_COUNT_EXTRA"
+        private const val TOTAL_COUNT_EXTRA = "TOTAL_COUNT_EXTRA"
+
         fun getIntent(context: Context, totalCount: Int): Intent {
             return Intent(context, DetailsActivity::class.java).apply {
                 putExtra(TOTAL_COUNT_EXTRA, totalCount)
             }
         }
-
     }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        presenter.onDetach()
-    }
-
-
-
 }
